1f475ae19462f95e417b1584435a0fd4ce4cdd38
itcast <danni@itcast.cn>
2020-02-18 22:27:57 +0800

    分类信息提交

--- static/js/hot.js
+++ static/js/hot.js
@@ -4,7 +4,6 @@ $.ajax({
     url: 'http://localhost:8080/api/v1/index/rank',
     success: function (response) {
         // console.log(response);
-
         var weekTpl = `
 {{each data}}
 <li>
@@ -12,7 +11,6 @@ $.ajax({
 <a href="/article.html?id={{$value.id}}">{{$value.title}}</a>
 </li>
 {{/each}}`;
-        
         var html = template.render(weekTpl, response);
         // console.log(html);
         $('#weekBox').html(html);
--- static/js/hotpic.js
+++ static/js/hotpic.js
@@ -3,7 +3,7 @@ $.ajax({
     type: 'get',
     url: 'http://localhost:8080/api/v1/index/hotpic',
     success: function (data) {
-        console.log(data);
+        // console.log(data);
         var html = template('focusTpl', { data: data.data });
         $('#focusBox').html(html);
         $('#focusBox li:first-child').addClass('first');
