c112a341ebfdd7a3ef1e99e10db0a76a078863c7
itcast <danni@itcast.cn>
2020-02-18 16:47:57 +0800

    一周热门排行第一次提交

--- static/js/hot.js
+++ static/js/hot.js
@@ -1,17 +1,18 @@
-// 获取热门推荐数据
+// 获取热门排行数据
 $.ajax({
     type: 'get',
     url: 'http://localhost:8080/api/v1/index/rank',
     success: function (response) {
         console.log(response);
+
         var weekTpl = `
 {{each data}}
 <li>
 <span class="{{if $index==0}}first {{else if $index==1}}second{{else if $index==2}}third{{/if}}">{{$index+1}}</span>
-<a href="javascript:;">{{$value.title}}</a>
+<a href="/article.html?id={{$value.id}}">{{$value.title}}</a>
 </li>
 {{/each}}`;
-        console.log();
+        
         var html = template.render(weekTpl, response);
         // console.log(html);
         $('#weekBox').html(html);
